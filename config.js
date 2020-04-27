6b6ea91bfed54909548ad880ee6dc7a90d1992d1
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-04-27 14:37:34 +0100

    added a database

--- config.js
+++ config.js
@@ -7,4 +7,13 @@ module.exports = {
     DF_LANGUAGE_CODE: process.env.DF_LANGUAGE_CODE,
     GOOGLE_CLIENT_EMAIL: process.env.GOOGLE_CLIENT_EMAIL,
     GOOGLE_PRIVATE_KEY:process.env.GOOGLE_PRIVATE_KEY.replace(new RegExp("\\\\n", "\g"), "\n"),
+    PG_CONFIG: {
+      user: process.env.PG_CONFIG_USER,
+      database: process.env.PG_CONFIG_DATABASE,
+      password: process.env.PG_CONFIG_PASSWORD,
+      host: process.env.PG_CONFIG_HOST,
+      port: 5432,
+      max: 10,
+      idleTimeoutMillis: 30000,
+    }
 };
