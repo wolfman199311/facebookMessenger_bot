69c2aeca2a59c8f3a71d1e7f987a8502c0fb176e
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-04-29 14:52:42 +0100

    fixing ssl error

--- user.js
+++ user.js
@@ -4,7 +4,7 @@ const config = require('./config');
 const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: false,
 });
 
 client.connect();
