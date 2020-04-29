1a4156abbe1ae21f894e6cb01a278c49dd5b4167
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-04-29 21:04:14 +0100

    formatting

--- user.js
+++ user.js
@@ -19,8 +19,10 @@ module.exports = {
                 access_token: config.FB_PAGE_TOKEN
             }
 
-        }, function (error, response, body) {
+        },
+         function (error, response, body) {
             if (!error && response.statusCode == 200) {
+
                 var user = JSON.parse(body);
                 if (user.first_name.length > 0) {
                     var pool = new pg.Pool(config.PG_CONFIG);
@@ -35,7 +37,7 @@ module.exports = {
                                     console.log('Query error: ' + err);
                                 } else {
                                     if (result.rows.length === 0) {
-                                        let sql = 'INSERT INTO users (fb_id, first_name, last_name, pro) ' +
+                                        let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_picture) ' +
                                             'VALUES ($1, $2, $3, $4)';
                                         client.query(sql,
                                             [
