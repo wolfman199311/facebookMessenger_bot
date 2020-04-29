9c0279e8704587b55e7dd97b4c1254ea65adbe9d
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-04-29 15:47:44 +0100

    pg

--- user.js
+++ user.js
@@ -2,7 +2,6 @@
 const request = require('request');
 const config = require('./config');
 const {Client}= require('pg');
-const {Client}= 'pg';
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: false,
