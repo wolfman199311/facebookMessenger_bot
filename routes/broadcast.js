2e6cf48205dda46fcc066555cd357604247df720
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-05-11 15:30:35 +0100

    another dot

--- routes/broadcast.js
+++ routes/broadcast.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 const express = require('express');
 const userService = require('../user');
-const fbService = require('./fb-service');
+const fbService = require('../fb-service');
 const router = express.Router();
 
 
