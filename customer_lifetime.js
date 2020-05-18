55665d480336c14efbed976a7abf7df6f03b32b2
Wolf Man <wolfman199311@gmail.com>
2020-05-18 23:03:04 +0300

    delay1

--- customer_lifetime.js
+++ customer_lifetime.js
@@ -83,7 +83,7 @@ module.exports = {
                                
                                 results.forEach(async (item, index, array) => {
                                     i++;
-                                    await delay(i*50);
+                                    await delay(i*150);
                                     console.log(array.length, item);
                                     fbService.sendTextMessage(userId, item);
                                     
