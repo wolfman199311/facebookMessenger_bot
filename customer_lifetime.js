0a04f24c871e56c17a7f86769c68cbc0ecad0276
Wolf Man <wolfman199311@gmail.com>
2020-05-18 11:05:00 +0300

    complete1

--- customer_lifetime.js
+++ customer_lifetime.js
@@ -20,161 +20,176 @@ const pool = new Pool({
     port: 5432
 });
 
+// var tablename = 'csvData' + userId;
+
+// var url = "https://cdn.fbsbx.com/v/t59.2708-21/97244361_173244120675140_9216578399319883776_n.xlsx/Book1-3.xlsx?_nc_cat=111&_nc_sid=0cab14&_nc_ohc=2nu_VI6RsZoAX928D3A&_nc_ht=cdn.fbsbx.com&oh=8683367f97c0f3ad13768cac98d764d1&oe=5EC2B467";
+
 module.exports = {
 
-    saveData: function (callback, csv_url, userId) {
+    pythonpy: function (callback, url, userId) {
+        var tablename = 'csvData' + userId;
         let self = module.exports;
-
-        var req = https.get(url.parse(csv_url), function (res) {
-            if (res.statusCode !== 200) {
+        https.get(url, (res) => {
+            debugger
+            const {
+                statusCode
+            } = res;
+            const contentType = res.headers['content-type'];
+            console.log(`The type of the file is : ${contentType}`)
+            let error;
+            if (statusCode !== 200) {
+                error = new Error(`Request Failed.\n` +
+                    `Status Code: ${statusCode}`);
+            }
+            if (error) {
+                console.error(error.message);
+                // consume response data to free up memory
+                res.resume();
                 return;
             }
-            var data = [], dataLen = 0;
-            var csvData = [];
-            res.on("data", function (chunk) {
-                data.push(chunk);
-                dataLen += chunk.length;
+            res.setEncoding('binary');
+            let rawData = '';
+            res.on('data', (chunk) => {
+                rawData += chunk;
             });
+            res.on('end', () => {
+                try {
+                    // And / Or just put it in a file
+                    var xlsxfilename = tablename + '.xlsx';
+                    fs.writeFile(xlsxfilename, rawData, 'binary', async (err) => {
+                        if (err) throw err;
+                        console.log("Successfully saved XLSX file.");
+                        // const parsedData = await xlsxToCSVFunction(rawData);
+                        var setting = self.saveData.bind(null, tablename);
+                        callback(setting);
+                        console.log("Write to bezkoder_postgresql_fs.csv successfully!");
+                        var command = '././' + xlsxfilename;
+                        var comport = 6;
 
-            var tablename = 'csvData' + userId;
-            const queryCreat =
-                'CREATE TABLE ' + tablename + '(id SERIAL PRIMARY KEY, invoiceno varchar(450) NOT NULL, stockcode varchar(450) NOT NULL, description varchar(450) NOT NULL, quantity varchar(450) NOT NULL, invoicedate varchar(450) NOT NULL, unitprice varchar(450) NOT NULL, customerid varchar(450) NOT NULL, country varchar(450) NOT NULL)';
-            console.log(queryCreat);
-            const query =
-                'INSERT INTO csvData' + userId + '(InvoiceNo, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
-            res.on("end", function () {
-                var buf = Buffer.concat(data);
-                var datas = buf.toString();
-                var str = datas.split("\n");
-                // remove the first line: header
-                str.shift();
-                str.forEach(rows => {
-                    var element = rows.split(",");
-                    if (rows != "") {
-                        csvData.push(element);
-                    }
-                })
+                        var options = {
+                            scriptPath: 'python/',
+                            args: [command, comport], // pass arguments to the script here
+                        };
 
-                pool.connect((err, client, done) => {
-                    if (err) throw err;
-
-                    try {
-                        //csvData.forEach(row => {
-                        client.query(queryCreat, (err, res) => {
+                        PythonShell.run('script.py', options, function (err, results) {
                             if (err) {
-                                console.log("table already be created");
+                                console.log(err + "That is definitely not Excel .xls format. Open it with a text editor (e.g. Notepad) that won't take any notice of the (incorrect) .xls extension and see for yourself.");
+
+                            } else {
+                                console.log('results: %j', results);
+                                results.forEach(item => {
+                                    var i = 0;
+                                    if (i < 10) {
+                                        console.log(item);
+                                        fbService.sendTextMessage(IDname, item);
+                                        i++;
+                                    }
+                                });
                             }
-                            pool.connect((err, client, done) => {
-                                if (err) callback(false);
-                                try {
 
-                                    client.query("DELETE FROM category", (err, res) => {
-                                        if (err) throw err;
-                                        csvData.forEach(row => {
-                                            client.query(query, row, (err, res) => {
-                                                if (err) {
-                                                    console.log(err.stack);
-                                                } else {
-                                                    console.log("inserted " + res.rowCount + " row:", row);
-                                                }
-                                            });
-                                        });
-                                    });
-                                    callback(true);
-                                    
-                                    console.log("display result")
-                                } finally {
-                                    console.log("display python script part")
-                                    done();
-                                }
-                            });
                         });
-                    } finally {
-                        done();
-                    }
-                });
-                setTimeout(self.csvwriter.bind(null, userId, tablename), 5000);
+
+                    });
+                    // console.log(parsedData);
+                } catch (e) {
+                    callback(false);
+                    console.error(e.message);
+                }
             });
-
-        });
-
-        req.on("error", function (err) {
-            // handle error
-            console.log(error);
+        }).on('error', (e) => {
+            callback(false);
+            console.error(`Got error: ${e.message}`);
         });
 
     },
-    csvwriter: function (IDname, tablename) {
-        // console.log(IDname);
-        console.log(tablename);
-        var filefullname = tablename + '.xlsx';
-        const ws = fs.createWriteStream(filefullname);
 
-        pool.connect((err, client, done) => {
-            if (err) throw err;
-            var selectquery =
-                'SELECT * FROM ' + tablename;
-            client.query(selectquery, (err, res) => {
-                done();
+    saveData: function (rawData, tablename) {
+        const queryDelete = 'DELETE FROM ' + tablename;
+        const queryCreat =
+            'CREATE TABLE ' + tablename + '(id SERIAL PRIMARY KEY, invoiceno varchar(450) NOT NULL, stockcode varchar(450) NOT NULL, description varchar(450) NOT NULL, quantity varchar(450) NOT NULL, invoicedate varchar(450) NOT NULL, unitprice varchar(450) NOT NULL, customerid varchar(450) NOT NULL, country varchar(450) NOT NULL)';
+        const query =
+            'INSERT INTO ' + tablename + '(InvoiceNo, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
 
-                if (err) {
-                    console.log(err.stack);
-                } else {
-                    const jsonData = JSON.parse(JSON.stringify(res.rows));
-                    // console.log("jsonData", jsonData);
 
-                    var xls = json2xls(jsonData);
-                    // var tempFile = fs.openSync(filefullname, 'r');
-                    // // try commenting out the following line to see the different behavior
-                    // fs.closeSync(tempFile);
+        var xlsxfilename = tablename + '.xlsx';
+        var obj = xlsx.parse(xlsxfilename); // parses a file
+        var rows = [];
+        var writeStr = "";
 
-                    // fs.unlinkSync(filefullname);
-                    fs.unlink(filefullname, (err) => {
-                        if (err) {
-                            console.log("failed to delete local image:" + err);
-                        } 
-                            console.log('successfully deleted local file');
-                            fs.writeFile(filefullname, xls, function (error) {
-                                if (error) throw error;
-                                console.log("Write to bezkoder_postgresql_fs.csv successfully!");
-                                var command = '././' + filefullname;
-                                var comport = 6;
+        //looping through all sheets
+        for (var i = 0; i < obj.length; i++) {
+            var sheet = obj[i];
+            //loop through all rows in the sheet
+            for (var j = 0; j < sheet['data'].length; j++) {
+                //add the row to the rows array
+                rows.push(sheet['data'][j]);
+            }
+        }
 
-                                var options = {
-                                    scriptPath: 'python/',
-                                    args: [command, comport], // pass arguments to the script here
-                                };
+        //creates the csv string to write it to a file
+        for (var i = 0; i < rows.length; i++) {
+            writeStr += rows[i].join(",") + "\n";
+        }
 
-                                PythonShell.run('script.py', options, function (err, results) {
-                                    if (err) {
-                                        console.log("That is definitely not Excel .xls format. Open it with a text editor (e.g. Notepad) that won't take any notice of the (incorrect) .xls extension and see for yourself.");
+        //writes to a file, but you will presumably send the csv as a      
+        //response instead
+        var csvfilename = tablename + '.csv';
+        fs.writeFile(csvfilename, writeStr, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("filefullname.csv was saved in the current directory!");
+            let stream = fs.createReadStream(csvfilename);
+            let csvData = [];
+            let csvStream = fastcsv
+                .parse()
+                .on("data", function (data) {
+                    csvData.push(data);
+                })
+                .on("end", function () {
+                    // remove the first line: header
+                    csvData.shift();
 
-                                    } else {
-                                        console.log('results: %j', results);
-                                        results.forEach(item => {
-                                            console.log(item);
-                                            var i = 0;
-                                            if (i < 10) {
-                                                fbService.sendTextMessage(IDname, item);
-                                                i++;
+                    pool.connect((err, client, done) => {
+                        try {
+                            client.query(queryCreat)
+                                .then((err, res) => {
+                                    csvData.forEach(row => {
+                                        client.query(query, row, (err, res) => {
+                                            if (err) throw err;
+                                            else {
+                                                console.log("inserted ${res.rowCount} row , ${row}");
+
                                             }
-                                            fbService.sendTextMessage(IDname, item);
                                         });
-                                    }
+                                    });
+                                }).catch(e => {
+                                    console.log(e);
+                                    client.query(queryDelete)
+                                        .then((err, res) => {
+                                            console.log("delete database");
+                                            //insert record.
+                                            csvData.forEach(row => {
+                                                client.query(query, row, (err, res) => {
+                                                    if (err) throw err;
+                                                    else {
+                                                        console.log("inserted ${res.rowCount} row , ${row}");
 
+                                                    }
+                                                });
+                                            });
+                                        });
                                 });
+                        } catch (err) {
+                            console.log("err");
+                        }
+                    })
 
-                            });
-                        
-                    });
+                });
 
-
-
-
-                }
-            });
+            stream.pipe(csvStream);
         });
 
 
-    }
+    },
 }
--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-const Pool = require("pg").Pool;
-const Json2csvParser = require("json2csv").Parser;
+
+"use strict";
 const fs = require("fs");
-var json2xls = require('json2xls');
-// Create a connection to the database
+const Pool = require("pg").Pool;
+const fastcsv = require("fast-csv");
+var xlsx = require('node-xlsx');
+var https = require("https");
+var url = require("url");
+
+// create a new connection to the database
 const pool = new Pool({
     host: "localhost",
     user: "postgres",
@@ -11,44 +16,169 @@ const pool = new Pool({
     port: 5432
 });
 
-// open the PostgreSQL connection
-pool.connect((err, client, done) => {
-    if (err) throw err;
+const userId = "42245252";
+var tablename = 'csvData' + userId;
+const queryDelete = 'DELETE FROM ' + tablename;
+const queryCreat =
+    'CREATE TABLE ' + tablename + '(id SERIAL PRIMARY KEY, invoiceno varchar(450) NOT NULL, stockcode varchar(450) NOT NULL, description varchar(450) NOT NULL, quantity varchar(450) NOT NULL, invoicedate varchar(450) NOT NULL, unitprice varchar(450) NOT NULL, customerid varchar(450) NOT NULL, country varchar(450) NOT NULL)';
+console.log(queryCreat);
+const query =
+    'INSERT INTO csvData' + userId + '(InvoiceNo, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
 
-    client.query("SELECT * FROM category", (err, res) => {
-        done();
+var url = "https://cdn.fbsbx.com/v/t59.2708-21/97244361_173244120675140_9216578399319883776_n.xlsx/Book1-3.xlsx?_nc_cat=111&_nc_sid=0cab14&_nc_ohc=2nu_VI6RsZoAX928D3A&_nc_ht=cdn.fbsbx.com&oh=8683367f97c0f3ad13768cac98d764d1&oe=5EC2B467";
 
-        if (err) {
-            console.log(err.stack);
-        } else {
-            const jsonData = JSON.parse(JSON.stringify(res.rows));
-            console.log("jsonData", jsonData);
+https.get(url, (res) => {
+    debugger
+    const {
+        statusCode
+    } = res;
+    const contentType = res.headers['content-type'];
+    console.log(`The type of the file is : ${contentType}`)
+    let error;
+    if (statusCode !== 200) {
+        error = new Error(`Request Failed.\n` +
+            `Status Code: ${statusCode}`);
+    }
+    if (error) {
+        console.error(error.message);
+        // consume response data to free up memory
+        res.resume();
+        return;
+    }
+    res.setEncoding('binary');
+    let rawData = '';
+    res.on('data', (chunk) => {
+        rawData += chunk;
+    });
+    res.on('end', () => {
+        try {
+            // And / Or just put it in a file
+            var xlsxfilename = tablename + '.xlsx';
+            fs.writeFile(xlsxfilename, rawData, 'binary', (err) => {
+                if (err) throw err;
+                console.log("Successfully saved XLSX file.");
+                const parsedData = xlsxToCSVFunction(rawData);
+                console.log("Write to bezkoder_postgresql_fs.csv successfully!");
+                var command = '././' + filefullname;
+                var comport = 6;
 
-            const json2csvParser = new Json2csvParser({ header: true });
-            // const csv = json2csvParser.parse(jsonData);
+                var options = {
+                    scriptPath: 'python/',
+                    args: [command, comport], // pass arguments to the script here
+                };
 
-            // fs.writeFile("www.xlsx", xlsx, function(error) {
-            //   if (error) throw error;
-            //   console.log("Write to bezkoder_postgresql_fs.csv successfully!");
-            var xls = json2xls(jsonData);
-            // var filename = "qqq.xlsx";
-            // var tempFile = fs.openSync(filename, 'r');
-            // // try commenting out the following line to see the different behavior
-            // fs.closeSync(tempFile);
+                PythonShell.run('script.py', options, function (err, results) {
+                    if (err) {
+                        console.log(err + "That is definitely not Excel .xls format. Open it with a text editor (e.g. Notepad) that won't take any notice of the (incorrect) .xls extension and see for yourself.");
 
-            // fs.unlinkSync(filename, );
-            fs.unlink("www.xlsx", (err) => {
-                if (err) {
-                    console.log("failed to delete local image:" + err);
-                }
-                console.log('successfully deleted local image');
-                fs.writeFile("qqq.xlsx", xls, function (error) {
-                    if (error) throw error;
-                    console.log("Write to bezkoder_postgresql_fs.csv successfully!");
+                    } else {
+                        console.log('results: %j', results);
+                        results.forEach(item => {
+                            console.log(item);
+                            var i = 0;
+                            if (i < 10) {
+                                fbService.sendTextMessage(IDname, item);
+                                i++;
+                            }
+                            fbService.sendTextMessage(IDname, item);
+                        });
+                    }
+
                 });
 
             });
-            // fs.writeFileSync('www.xlsx', xls, 'binary');
+            // console.log(parsedData);
+        } catch (e) {
+            console.error(e.message);
         }
     });
+}).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
 });
+
+
+function xlsxToCSVFunction(rawData) {
+    // return rawData //you should return the csv file here whatever your tools are
+    var obj = xlsx.parse("filefullname.xlsx"); // parses a file
+    var rows = [];
+    var writeStr = "";
+
+    //looping through all sheets
+    for (var i = 0; i < obj.length; i++) {
+        var sheet = obj[i];
+        //loop through all rows in the sheet
+        for (var j = 0; j < sheet['data'].length; j++) {
+            //add the row to the rows array
+            rows.push(sheet['data'][j]);
+        }
+    }
+
+    //creates the csv string to write it to a file
+    for (var i = 0; i < rows.length; i++) {
+        writeStr += rows[i].join(",") + "\n";
+    }
+
+    //writes to a file, but you will presumably send the csv as a      
+    //response instead
+    var csvfilename = tablename + '.csv';
+    fs.writeFile(csvfilename, writeStr, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("filefullname.csv was saved in the current directory!");
+        let stream = fs.createReadStream("filefullname.csv");
+        let csvData = [];
+        let csvStream = fastcsv
+            .parse()
+            .on("data", function (data) {
+                csvData.push(data);
+            })
+            .on("end", function () {
+                // remove the first line: header
+                csvData.shift();
+                // console.log(csvData);
+
+                // connect to the PostgreSQL database
+                // save csvData
+
+                pool.connect((err, client, done) => {
+                    try {
+                        client.query(queryCreat)
+                            .then((err, res) => {
+                                csvData.forEach(row => {
+                                    client.query(query, row, (err, res) => {
+                                        if (err) throw err;
+                                        else {
+                                            console.log("inserted ${res.rowCount} row , ${row}");
+
+                                        }
+                                    });
+                                });
+                            }).catch(e => {
+                                console.log(e);
+                                client.query(queryDelete)
+                                    .then((err, res) => {
+                                        console.log("delete database");
+                                        //insert record.
+                                        csvData.forEach(row => {
+                                            client.query(query, row, (err, res) => {
+                                                if (err) throw err;
+                                                else {
+                                                    console.log("inserted ${res.rowCount} row , ${row}");
+
+                                                }
+                                            });
+                                        });
+                                    });
+                            });
+                    } catch (err) {
+                        console.log("err");
+                    }
+                })
+
+            });
+
+        stream.pipe(csvStream);
+    });
+}
+
