f49d8b28b571abaf9ed58adf28fe074453a01275
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-05-19 14:52:48 +0100

    done

--- helper-function/google-dialogflow.js
+++ helper-function/google-dialogflow.js
@@ -17,10 +17,16 @@ const credentials = {
   let four= 'projects/businessgrowthmentor-lgxlwf/knowledgeBases/OTA3ODk2ODc3NjczMjQ0MjYyNA';
   let five= 'projects/businessgrowthmentor-lgxlwf/knowledgeBases/MTgzMTkyMjkzMTIxODk4NTc3OTI';
 
+  const config = {
+      credentials: {
+          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(new RegExp("\\\\n", "\g"), "\n"),
+          client_email: process.env.GOOGLE_CLIENT_EMAIL
+      }
+  }
 
 
 // Create a session client
-const sessionClient = new dialogflow.SessionsClient(credentials)
+const sessionClient = new dialogflow.SessionsClient(config)
 
 const detectIntent = async (queryText, sessionId) => {
 
