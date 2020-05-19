b10df7e523e96d2cc8e239590e971e0cc92177fd
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-05-19 14:46:35 +0100

    credentials

--- helper-function/google-dialogflow.js
+++ helper-function/google-dialogflow.js
@@ -10,7 +10,7 @@ const credentials = {
 
 // KnowledgeBasePath
 // Sample path
- 
+
   let one= 'projects/businessgrowthmentor-lgxlwf/knowledgeBases/ODcyNjU2MjEwNTg5MDcwMTMxMg';
   let two= 'projects/businessgrowthmentor-lgxlwf/knowledgeBases/MTM2OTA2NTQ3OTUxNTk4MzA1Mjg';
   let three= 'projects/businessgrowthmentor-lgxlwf/knowledgeBases/NDI0Mjk0NzIwMTg2NjY2MTg4OA';
@@ -20,7 +20,7 @@ const credentials = {
 
 
 // Create a session client
-const sessionClient = new dialogflow.SessionsClient(config)
+const sessionClient = new dialogflow.SessionsClient(credentials)
 
 const detectIntent = async (queryText, sessionId) => {
 
