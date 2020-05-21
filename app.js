93d5e89e369bd14ea57abcf5f24aacafb9a3d82a
ShamailaHussain <44375912+ShamailaHussain@users.noreply.github.com>
2020-05-21 20:35:46 +0100

    Bl

--- app.js
+++ app.js
@@ -197,46 +197,6 @@ function setSessionAndUser(senderID) {
     }
 }
 
-const dateTimeToString = (date, time) => {
-
-    let year = date.split('T')[0].split('-')[0];
-    let month = date.split('T')[0].split('-')[1];
-    let day = date.split('T')[0].split('-')[2];
-
-    let hour = time.split('T')[1].split(':')[0];
-    let minute = time.split('T')[1].split(':')[1];
-
-    let newDateTime = `${year}-${month}-${day}T${hour}:${minute}`;
-
-    let event = new Date(Date.parse(newDateTime));
-
-    let options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-
-    return event.toLocaleDateString('en-US', options);
-};
-
-// Get date-time string for calender
-const dateTimeForCalander = (date, time) => {
-
-    let year = date.split('T')[0].split('-')[0];
-    let month = date.split('T')[0].split('-')[1];
-    let day = date.split('T')[0].split('-')[2];
-
-    let hour = time.split('T')[1].split(':')[0];
-    let minute = time.split('T')[1].split(':')[1];
-
-    let newDateTime = `${year}-${month}-${day}T${hour}:${minute}:00.000${TIMEOFFSET}`;
-
-    let event = new Date(Date.parse(newDateTime));
-
-    let startDate = event;
-    let endDate = new Date(new Date(startDate).setHours(startDate.getHours()+1));
-
-    return {
-        'start': startDate,
-        'end': endDate
-    }
-};
 
 function receivedMessage(event) {
 
